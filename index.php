12a650b8800cace80fac25d8564c24a0242ac65a
Your Name <you@example.com>
2025-07-18 15:21:48 +0700

    Base MVC

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-
 require_once 'app.php';
 require_once 'router.php';
+
+session_start();
 
 $app = new App();
 $app->setRoutes($routes);
--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,5 +1,22 @@
 <?php
 
+require_once './config.php';
+
 class BaseModel
 {
+  private $host = DB_HOST;
+  private $username = DB_USER;
+  private $password = DB_PASS;
+  private $dbname = DB_NAME;
+
+  protected $db;
+
+  public function __construct()
+  {
+    try {
+      $this->db = new PDO("mysql:host={$this->host};dbname={$this->dbname};charset=utf-8", $this->username, $this->password);
+    } catch (Exception $e) {
+      die('Lỗi kết nối cơ sở dữ liệu');
+    }
+  }
 }
