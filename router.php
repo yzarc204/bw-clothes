81013c22cff879ad3eb5b808e913a5dcc4822c82
nghĩa <nghia08092005@gmail.com>
2025-07-22 20:19:23 +0700

    Fix conflict

--- router.php
+++ router.php
@@ -5,12 +5,14 @@ $routes = [
   '^login$' => ['Auth', 'login'],
   '^register$' => ['Auth', 'register'],
   '^shop$' => ['Home', 'shop'],
+  'product/(\d+)' => ['Home', 'product'],
+  'cart' => ['Cart', 'index'],
+  'cart/add/(\d+)' => ['Cart', 'addToCart'],
+  'cart/remove/(\d+)' => ['Cart', 'remove'],
+  'cart/update' => ['Cart', 'update'],
+  'search' => ['Home', 'search'],
 ];
 
 $adminRoutes = [
   '^admin$' => ['Admin/Dashboard', 'index'],
-];
-
-$categoryRoutes = [
-  '^admin/category/create$' => ['Admin/Category', 'create'],
 ];
